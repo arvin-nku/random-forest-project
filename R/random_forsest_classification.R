f2d82c2cc1b2199206f9e27928162b5ac0b95a52
Lorenz4123 <lorenz.kiesel@web.de>
2024-08-10 08:21:40 +0200

    Beispiele angepasst (einheitlicher) und Code nochmal überprüft

--- R/random_forsest_classification.R
+++ R/random_forsest_classification.R
@@ -29,12 +29,10 @@
 #' @examples
 #' X1 <- runif(100, 0, 1)
 #' X2 <- runif(100, 0, 1)
-#' X3 <- runif(100, 0, 1)
-#' X4 <- runif(100, 0, 1)
-#' Y <- 2 * X1 + 3 * X2 + 3 * X3 + 4 * X4
-#' data <- list(x = matrix(c(X1, X2, X3, X4), nrow = 4, byrow = TRUE), y = Y)
+#' e <- rnorm(100, 0, 0.1)
+#' Y <- X1^2 + X2+ e
+#' data <- list(x = matrix(c(X1, X2), nrow = 2, byrow = TRUE), y = Y)
 #' random_forest_regression(data, B = 10, A = 20, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1)
-
 
 random_forest_regression <- function(data, B, A = NULL, m = 0, num_leaf = NULL,
                                      depth = NULL, num_split = 2, min_num = 1){
@@ -214,12 +212,11 @@ random_forest_regression <- function(data, B, A = NULL, m = 0, num_leaf = NULL,
 #' @examples
 #' X1 <- runif(100, 0, 1)
 #' X2 <- runif(100, 0, 1)
-#' X3 <- runif(100, 0, 1)
-#' X4 <- runif(100, 0, 1)
-#' Y <- ifelse(2 * X1 + 3 * X2 + 3 * X3 + 4 * X4 > 2.5, 1, 2)
-#' data <- list(x = matrix(c(X1, X2, X3, X4), nrow = 4, byrow = TRUE), y = Y)
-#' random_forest_classification(data, B = 10, A = 20, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1, unique = F)
-
+#' e <- rnorm(100, 0, 0.1)
+#' linear_comb <- 1.8 * X1 + 0.3 * X2 + e
+#' Y <- ifelse(linear_comb > 1, 1, 2)
+#' data <- list(x = matrix(c(X1, X2), nrow = 2, byrow = TRUE), y = Y)
+#' random_forest_classification(data, B = 5, A = 10, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1, unique = F)
 
 random_forest_classification <- function(data, B, A = NULL, m = 0, num_leaf = NULL, 
                                          depth = NULL, num_split = 2, min_num = 1, 
@@ -408,25 +405,21 @@ random_forest_classification <- function(data, B, A = NULL, m = 0, num_leaf = NU
 #' 
 #' @export
 #' 
-#' @examples 
+#' @examples
 #' X1 <- runif(100, 0, 1)
 #' X2 <- runif(100, 0, 1)
-#' X3 <- runif(100, 0, 1)
-#' X4 <- runif(100, 0, 1)
-#' e <- rnorm(50, 0, 0.1)
-#' Y <-  X1 + X2 + X3 + X4 + e
-#' data_reg <- tibble(a = X1, b = X2, c = X3, d = X4, y = Y)
-#' random_forest(x = c(a, b, c, d), y = Y, data = data_reg, type = "reg", B = 10, A = 20, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1, unique = F)
-#'
+#' e <- rnorm(100, 0, 0.1)
+#' Y <- X1^2 + X2+ e
+#' data_reg <- tibble(a = X1, b = X2, y = Y)
+#' random_forest(x = c(a, b), y = Y, data = data_reg, type = "reg", B = 10, A = 20, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1)
 #'
 #' X1 <- runif(100, 0, 1)
 #' X2 <- runif(100, 0, 1)
-#' X3 <- runif(100, 0, 1)
-#' X4 <- runif(100, 0, 1)
-#' Y <- ifelse(X1 + X2 + X3 + X4 > 2.5, 1, 2)
-#' data_cla <- list(a = X1, b = X2, c = X3, d = X4, y = Y)
-#' random_forest(x = c(a, b, c, d), y = Y, data = data_cla, type = "cla", B = 10, A = 20, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1, unique = F)
-
+#' e <- rnorm(100, 0, 0.1)
+#' linear_comb <- 1.8 * X1 + 0.3 * X2 + e
+#' Y <- ifelse(linear_comb > 1, 1, 2)
+#' data_cla <- list(a = X1, b= X2, y = Y)
+#' random_forest( x = c(a, b), y = Y, data = data_cla, type = "cla", B = 5, A = 10, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1, unique = F)
 
 random_forest <- function(x, y, data, type = NULL, B, A = NULL, m = 0,num_leaf = NULL, 
                           depth = NULL, num_split = 2, min_num = 1, unique = F ){
@@ -498,6 +491,8 @@ random_forest <- function(x, y, data, type = NULL, B, A = NULL, m = 0,num_leaf =
     stop("Invalid type: The type must be reg for regression or cla for classification")
   }
 }
+
+
 
 ### Beispiel und Anschauung
 
