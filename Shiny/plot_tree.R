855ffd4d709ca571928524140d3c73bb93648897
Lorenz4123 <lorenz.kiesel@web.de>
2024-08-12 17:55:22 +0200

    Plotfunktion in ShinyApp eingefügt. (Bei source jeder ändern bei überprüfung)

--- Shiny/plot_tree.R
+++ Shiny/plot_tree.R
@@ -70,4 +70,56 @@ install.packages("DiagrammeR")
 library(data.tree)
 library(dplyr)
 library(DiagrammeR)
-tree_plot(val) 
+tree_plot(val)
+
+tree_plot_random_forest <- function(data) {
+  
+  parent <- c()
+  child <- c()
+  
+  for (i in 1:nrow(data)) {
+    node_cur <- data[i, ]
+    
+    child_l <- data %>% filter(node == node_cur$node*2)
+    child_r <- data %>% filter(node == (node_cur$node*2)+1)
+    
+    if(nrow(child_l) > 0){
+      parent <- c(parent, paste0("Position: ", node_cur$node, "\n", "j = ",
+                                 node_cur$split_index, ", s = ", round(node_cur$split_point, digits = 2))
+      )
+      if(child_l$name == "leaf"){
+        child <- c(child, paste0("Position: ", child_l$node, "\n", "y = ",
+                                 round(child_l$c_value, digits = 2))
+        )
+      } else {
+        child <- c(child, paste0("Position: ", child_l$node, "\n", "j = ",
+                                 child_l$split_index, ", s = ", round(child_l$split_point, digits = 2)))
+      }
+    }
+    
+    if(nrow(child_r) > 0){
+      parent <- c(parent, paste0("Position: ", node_cur$node, "\n", "j = ",
+                                 node_cur$split_index, ", s = ", round(node_cur$split_point, digits = 2))
+      )
+      if(child_r$name == "leaf"){
+        child <- c(child, paste0("Position: ", child_r$node, "\n", "y = ",
+                                 round(child_r$c_value, digits = 2))
+        )
+      } else {
+        child <- c(child, paste0("Position: ", child_r$node, "\n", "j = ",
+                                 child_r$split_index, ", s = ", round(child_r$split_point, digits = 2)))
+      }
+    }
+  }
+  
+  tree_data <- data.frame(parent = parent, child = child)
+  tree <- FromDataFrameNetwork(tree_data)
+  
+  # Grafikoptionen setzen
+  SetGraphStyle(tree, rankdir = "TB")
+  SetEdgeStyle(tree, arrowhead = "normal", arrowsize = 0.3)
+  SetNodeStyle(tree, style = "box", shape = "box", height = 0.05, fontsize = 8)
+  
+  # Plot erstellen
+  plot(tree)
+}
