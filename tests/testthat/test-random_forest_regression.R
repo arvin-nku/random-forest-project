624888fa7d735369a93c7f782adb33d0f5e431ba
Lorenz4123 <lorenz.kiesel@web.de>
2024-08-16 16:56:14 +0200

    Test random_forest_regression

--- tests/testthat/test-random_forest_regression.R
+++ tests/testthat/test-random_forest_regression.R
@@ -6,6 +6,7 @@ test_that("random_forest_regression works with valid input", {
   X2 <- runif(100, 0, 1)
   e <- rnorm(100, 0, 0.1)
   Y <- X1^2 + X2 + e
+  
   data <- list(x = matrix(c(X1, X2), nrow = 2, byrow = TRUE), y = Y)
   
   #function with valid input
@@ -24,32 +25,36 @@ test_that("random_forest_regression works with valid input", {
 
 test_that("random_forest_regression throws errors for invalid input", {
   
+  #Testdata
+  set.seed(123)
+  X1 <- runif(100, 0, 1)
+  X2 <- runif(100, 0, 1)
+  e <- rnorm(100, 0, 0.1)
+  Y <- X1^2 + X2 + e
+  
+  data <- list(x = matrix(c(X1, X2), nrow = 2, byrow = TRUE), y = Y)
+  
   #Invalid input:
   
-  #data
-  expect_error(random_forest_regression(data = list(x = 1, y = 1), B = 10), "data must be a list")
-  
   #B
-  expect_error(random_forest_regression(data, B = -1), "Number of bootstrap samples must be greater than or equal to 1")
+  expect_error(random_forest_regression(data, B = -1, A = 2), "Number of bootstrap samples must be greater than or equal to 1")
   
   #A
-  expect_warning(random_forest_regression(data, B = 10, A = 200), "A is too large")
-  expect_warning(random_forest_regression(data, B = 10, A = -5), "A must be greater 0")
+  expect_warning(random_forest_regression(data, B = 10, A = 200, m = 1), "A is too large")
+  expect_warning(random_forest_regression(data, B = 5, A = -5, m = 1), "A must be greater 0")
   
   #m
-  expect_warning(random_forest_regression(data, B = 10, m = -5), "m must be greater 0")
+  expect_warning(random_forest_regression(data, B = 5, A = 2, m = -5), "m must be greater 0")
   
   #num_leaf
-  expect_warning(random_forest_regression(data, B = 10, num_leaf = -1), "num_leaf must be greater than or equal to 1")
+  expect_warning(random_forest_regression(data, B = 5, A = 2, num_leaf = -1), "num_leaf must be greater than or equal to 1")
   
   #num_split
-  expect_warning(random_forest_regression(data, B = 10, num_split = 1), "num_split must be greater or equal to 2")
+  expect_warning(random_forest_regression(data, B = 5, A = 2, num_split = 1), "num_split must be greater or equal to 2")
   
   #min_num
-  expect_warning(random_forest_regression(data, B = 10, min_num = 0), "min_num must be greater than or equal to 1")
+  expect_warning(random_forest_regression(data, B = 5, A = 2, min_num = 0), "min_num must be greater than or equal to 1")
 })
-
-
 
 test_that("random_forest_regression uses bagging if m and A equal dimension and length", {
   
@@ -63,10 +68,6 @@ test_that("random_forest_regression uses bagging if m and A equal dimension and 
   
   #bagging mode
   expect_warning(random_forest_regression(data, B = 2, A = length(Y), m = nrow(data$x)), 
-                 "Bagging is used")
+                 "Bagging is used. To use Random Forest, enter a value less than 2 for m or a value less than 100 for A")
 })
-
-
-list.files("tests/testthat/")
-test_file("tests/testthat/test-random_forest_regression.R")
 
