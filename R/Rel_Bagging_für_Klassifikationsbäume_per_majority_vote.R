5ee4c45d4a2173c1bdcac26376d7bf7620338c47
arvincoded <nikouarvin@gmail.com>
2024-08-21 22:16:26 +0200

    Created additional Documentation in man/

--- R/Rel_Bagging_für_Klassifikationsbäume_per_majority_vote.R
+++ R/Rel_Bagging_für_Klassifikationsbäume_per_majority_vote.R
@@ -1,114 +1,114 @@
-#' Bagging Classification using majority vote method
-#' This script implements the Bagging classification algorithm using majority voting
-
-# Check and load required libraries
-if (!require(ggplot2)) install.packages("ggplot2")
-if (!require(cowplot)) install.packages("cowplot")
-
-library(ggplot2)
-library(cowplot)
-
-source("greedy_algorithm.R")
-source("main.R")
-
-# Function generates a bootstrap sample from the provided data. It randomly selects 
-# samples with replacement, creating a new dataset of the same size as the original:
-#' @param X Data frame or matrix of predictors.
-#' @param Y Vector of the response variable.
-#' @return List containing the bootstrap sample of predictors (`X`) and the corresponding response variable (`Y`).
-#' @export
-#' @example
-bootstrap_sample_classification <- function(X, Y) {
-  
-  
-  if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
-  {
-    stop("X must be list or matrix or data frame!")
-  }
-  
-  if (!is.vector(Y))
-  {
-    stop("Y must be vector!")
-  }
-  n <- ncol(X)
-  
-  if (n != length(Y))
-  {
-    stop("Dimension of x and y in data are not equal")
-  }
-  sample_indices <- sample(1:n, n, replace = TRUE)
-  return(list("X" = X[,sample_indices], "Y" = Y[sample_indices]))
-}
-
-# Function trains multiple classification models (decision trees) on different bootstrap 
-# samples. The number of models (`B`) can be specified:
-
-bagging_classification<- function(X, Y, B = 100, x_vector = NULL) {
-  #' @param X Data frame or matrix of predictors
-  #' @param Y Vector of the response variable
-  #' @param B Number of bootstrap samples/models to train (default is 100)
-  #' @return List of trained classification models
-  
-  n <- nrow(X)
-  models = list()
-  
-  for (i in 1:B) {
-    sample_data <- bootstrap_sample_classification(X, Y)
-    X_boot = sample_data$X
-    Y_boot = sample_data$Y
-    data <- list(x = X_boot, y = Y_boot)
-    #debug(greedy_cart_classification)
-    models[[i]] <- greedy_cart_classification(data = data, depth = 5) # Training a single decision tree on each bootstrap sample
-  }
-  
-  return(models)
-}
-
-# Function aggregates the predictions from all trained models using majority voting:
-
-bagging_classification_prediction <- function(models, X) {
-  #' @param models List of trained models from bagging_training
-  #' @param X Data frame or matrix of predictors to make predictions on
-  #' @return Vector of aggregated predictions using majority voting
-  
-  B = length(models)
-  n = nrow(X)
-  predictions <- matrix(0, n, B)
-  
-  for (i in 1:B) {
-    #print(length(prediction(list_tree = models[[i]], list_x = t(X), type = 'class')))
-    predictions[, i] <- prediction(list_tree = list(models[[i]]), list_x = t(X), type = 'cla')
-  }
-  # Use majority voting to determine the final prediction
-  bagged_predictions <- apply(predictions, 1, function(row) names(which.max(table(row))))
-  
-  return(bagged_predictions)
-}
-
-# Function visualizes the decision boundary created by the bagging model 
-# along with the actual data points:
-
-plot_decision_boundary <- function(data, models, title) {
-  #' @param data Data frame containing the original data points
-  #' @param models List of trained models from bagging_training
-  #' @param title Title for the plot
-  #' @return ggplot object showing the decision boundary and data points.
-  
-  x_seq <- seq(0, 1, length.out = 100)
-  grid <- expand.grid(x1 = x_seq, x2 = x_seq)
-  #print(bagging_prediction(models,grid))
-  grid$y <- bagging_classification_prediction(models, grid)
-  
-  ggplot(data, aes(x = x1, y = x2, color = y)) +
-    geom_point() +
-    geom_contour(data = grid, aes(z = as.numeric(y)), breaks = c(1.5), col = "black") +
-    ggtitle(title) +
-    theme_minimal()
-}
-
-#' @examples 
-#' models <- bagging_classification(X_train, Y_train, B = 1000)
-#' predictions <- bagging_prediction(models, X_test)
-
-# The script generates synthetic data for classification to demonstrate the bagging process:
-
+#' #' Bagging Classification using majority vote method
+#' #' This script implements the Bagging classification algorithm using majority voting
+#' 
+#' # Check and load required libraries
+#' if (!require(ggplot2)) install.packages("ggplot2")
+#' if (!require(cowplot)) install.packages("cowplot")
+#' 
+#' library(ggplot2)
+#' library(cowplot)
+#' 
+#' source("greedy_algorithm.R")
+#' source("main.R")
+#' 
+#' # Function generates a bootstrap sample from the provided data. It randomly selects 
+#' # samples with replacement, creating a new dataset of the same size as the original:
+#' #' @param X Data frame or matrix of predictors.
+#' #' @param Y Vector of the response variable.
+#' #' @return List containing the bootstrap sample of predictors (`X`) and the corresponding response variable (`Y`).
+#' #' @export
+#' #' @example
+#' bootstrap_sample_classification <- function(X, Y) {
+#'   
+#'   
+#'   if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
+#'   {
+#'     stop("X must be list or matrix or data frame!")
+#'   }
+#'   
+#'   if (!is.vector(Y))
+#'   {
+#'     stop("Y must be vector!")
+#'   }
+#'   n <- ncol(X)
+#'   
+#'   if (n != length(Y))
+#'   {
+#'     stop("Dimension of x and y in data are not equal")
+#'   }
+#'   sample_indices <- sample(1:n, n, replace = TRUE)
+#'   return(list("X" = X[,sample_indices], "Y" = Y[sample_indices]))
+#' }
+#' 
+#' # Function trains multiple classification models (decision trees) on different bootstrap 
+#' # samples. The number of models (`B`) can be specified:
+#' 
+#' bagging_classification<- function(X, Y, B = 100, x_vector = NULL) {
+#'   #' @param X Data frame or matrix of predictors
+#'   #' @param Y Vector of the response variable
+#'   #' @param B Number of bootstrap samples/models to train (default is 100)
+#'   #' @return List of trained classification models
+#'   
+#'   n <- nrow(X)
+#'   models = list()
+#'   
+#'   for (i in 1:B) {
+#'     sample_data <- bootstrap_sample_classification(X, Y)
+#'     X_boot = sample_data$X
+#'     Y_boot = sample_data$Y
+#'     data <- list(x = X_boot, y = Y_boot)
+#'     #debug(greedy_cart_classification)
+#'     models[[i]] <- greedy_cart_classification(data = data, depth = 5) # Training a single decision tree on each bootstrap sample
+#'   }
+#'   
+#'   return(models)
+#' }
+#' 
+#' # Function aggregates the predictions from all trained models using majority voting:
+#' 
+#' bagging_classification_prediction <- function(models, X) {
+#'   #' @param models List of trained models from bagging_training
+#'   #' @param X Data frame or matrix of predictors to make predictions on
+#'   #' @return Vector of aggregated predictions using majority voting
+#'   
+#'   B = length(models)
+#'   n = nrow(X)
+#'   predictions <- matrix(0, n, B)
+#'   
+#'   for (i in 1:B) {
+#'     #print(length(prediction(list_tree = models[[i]], list_x = t(X), type = 'class')))
+#'     predictions[, i] <- prediction(list_tree = list(models[[i]]), list_x = t(X), type = 'cla')
+#'   }
+#'   # Use majority voting to determine the final prediction
+#'   bagged_predictions <- apply(predictions, 1, function(row) names(which.max(table(row))))
+#'   
+#'   return(bagged_predictions)
+#' }
+#' 
+#' # Function visualizes the decision boundary created by the bagging model 
+#' # along with the actual data points:
+#' 
+#' plot_decision_boundary <- function(data, models, title) {
+#'   #' @param data Data frame containing the original data points
+#'   #' @param models List of trained models from bagging_training
+#'   #' @param title Title for the plot
+#'   #' @return ggplot object showing the decision boundary and data points.
+#'   
+#'   x_seq <- seq(0, 1, length.out = 100)
+#'   grid <- expand.grid(x1 = x_seq, x2 = x_seq)
+#'   #print(bagging_prediction(models,grid))
+#'   grid$y <- bagging_classification_prediction(models, grid)
+#'   
+#'   ggplot(data, aes(x = x1, y = x2, color = y)) +
+#'     geom_point() +
+#'     geom_contour(data = grid, aes(z = as.numeric(y)), breaks = c(1.5), col = "black") +
+#'     ggtitle(title) +
+#'     theme_minimal()
+#' }
+#' 
+#' #' @examples 
+#' #' models <- bagging_classification(X_train, Y_train, B = 1000)
+#' #' predictions <- bagging_prediction(models, X_test)
+#' 
+#' # The script generates synthetic data for classification to demonstrate the bagging process:
+#' 
--- R/Rel_Bagging_für_Regressionsbäume.R
+++ R/Rel_Bagging_für_Regressionsbäume.R
@@ -1,157 +1,157 @@
-#' Bagging regression
-#' This script implements a Bagging approach for regression 
-
-source("greedy_algorithm.R")
-source("main.R")
-
-# Function generates a bootstrap sample from the provided data. It randomly selects 
-# samples with replacement, creating a new dataset of the same size as the original:
-#' @param X Data frame or matrix of predictors.
-#' @param Y Vector of the response variable.
-#' @return List containing the bootstrap sample of predictors (`X`) and the corresponding response variable (`Y`).
-#' @export
-#' @example
-bootstrap_sample_regression <- function(X, Y) {
-  if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
-  {
-    stop("X must be list or matrix or data frame!")
-  }
-  
-  if (!is.vector(Y))
-  {
-    stop("Y must be vector!")
-  }
-  n <- ncol(X)
-  if (n != length(Y))
-  {
-    stop("Dimension of x and y in data are not equal")
-  }
-  sample_indices <- sample(1:n, n, replace = TRUE)
-  return(list("X" = X[, sample_indices, drop = F], "Y" = Y[sample_indices]))
-}
-
-# Function trains multiple regression models (decision trees) on different bootstrap 
-# samples. The number of models (`B`) can be specified:
-
-#' @param X Data frame or matrix of predictors
-#' @param Y Vector of the response variable
-#' @param B Number of bootstrap samples/models to train (default is 100)
-#' @return List of trained regression models.
-#' @export
-#' @example
-
-bagging_regression <- function(X, Y, B = 100) {
-  
-  if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
-  {
-    stop("X must be list or matrix or data frame!")
-  }
-  
-  if (!is.vector(Y))
-  {
-    stop("Y must be vector!")
-  }
-  
-  if(as.integer(B)!= B){
-    warning("B is not an integer. The value of B is set to ", trunc(B))
-    B <- trunc(B)
-  }
-  if(B < 1){
-    stop("Number of bootstrap samples must be greater than or equal to 1")
-  }
-  models <- list()
-  
-  
-  for (i in 1:B) {
-    #debug(bootstrap_sample)
-    sample_data <- bootstrap_sample_regression(X, Y)
-    X_boot = sample_data$X
-    Y_boot = sample_data$Y
-    #print(X_boot)
-    data <- list(x = X_boot, y = Y_boot)
-    model <- greedy_cart_regression(data, num_split = 5)# Training a single decision tree on each bootstrap sample
-    models[[i]] <- model
-  }
-  
-  return(models)
-}
-
-# Function aggregates the predictions from all trained models by averaging them:
-#' @param models List of trained models from bagging_regression
-#' @param X Data frame or matrix of predictors to make predictions on
-#' @return Vector of aggregated predictions.
-#' @export
-#' @example
-
-bagging_regression_prediction <- function(models, X) {
-
-  if (!is.list(models))
-  {
-    stop("Models must be list!")
-  }
-  
-  if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
-  {
-    stop("X must be list or matrix or data frame!")
-  }
-  #print(models)
-  
-  preds <- sapply(models, function(model) {
-    #debug(prediction)
-    prediction(list_tree = list(model), list_x = X, type = 'reg')
-  })
-  
-  #print(preds)
-  return(rowMeans(preds))
-}
-
-#' @examples
-#' models <- bagging_regression(X_train, Y_train, B = 1000)
-#' predictions <- bagging_prediction(models, X_test)
-
-# The script generates synthetic data to demonstrate the bagging regression process:
-
-#set.seed(123)
-#n <- 200
-#x <- runif(n, 0, 1)
-#y <- sin(2 * pi * x) + rnorm(n, 0, 0.1)
-#data <- list(x = matrix(x, nrow = 1, byrow = TRUE), y = y)
-
-
-# Two models are trained and visualized:
-# 
-# 1. Single Tree Model (`B = 1`):
-#    - Trains a single decision tree and visualizes the prediction against the true function.
-# 
-# 2. Bagging Model (`B = 1000`):
-#    - Trains 1000 decision trees and visualizes the aggregated prediction.
-
-# Extract predictors and response variable from the data
-#X <- data$x
-#Y <- data$y
-#print(X)
-#print(Y)
-# Plotting layout
-#par(mfrow = c(2, 2))
-
-# Training and plotting for B = 1 (Single Tree)
-#debug(random_forest)
-#debug(bootstrap_sample)
-#debug(find_best_split_regression)
-#single_model <- random_forest(x = x, y = y, type = 'reg', data = data, B = 1, A = 200, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1)#bagging_regression(X, Y, B = 1)
-#print("A")
-#print(single_model)
-#pred_single <- bagging_prediction(list(single_model), X)
-#plot(data$x, data$y, pch = 21, bg = 'lightgrey', main = "Bagging regression with 1 tree", xlab = "x1", ylab = "y")
-#lines(sort(data$x), pred_single[order(data$x)], col = 'blue')
-#lines(sort(data$x), sin(2 * pi * sort(data$x)), col = 'red')
-
-# Training and plotting for B = 1000 (Bagging)
-#models <- bagging_regression(X, Y, B = 1000)
-#pred_bagging <- bagging_prediction(models, X)
-#plot(data$x, data$y, pch = 21, bg = 'lightgrey', main = "Bagging regression with 1000 trees", xlab = "x1", ylab = "y")
-#lines(sort(data$x), pred_bagging[order(data$x)], col = 'blue')
-#lines(sort(data$x), sin(2 * pi * sort(data$x)), col = 'red')
-#print_forest_splits(models)
-# Resetting the plotting area
-#par(mfrow = c(1, 1))
+#' #' Bagging regression
+#' #' This script implements a Bagging approach for regression 
+#' 
+#' source("greedy_algorithm.R")
+#' source("main.R")
+#' 
+#' # Function generates a bootstrap sample from the provided data. It randomly selects 
+#' # samples with replacement, creating a new dataset of the same size as the original:
+#' #' @param X Data frame or matrix of predictors.
+#' #' @param Y Vector of the response variable.
+#' #' @return List containing the bootstrap sample of predictors (`X`) and the corresponding response variable (`Y`).
+#' #' @export
+#' #' @example
+#' bootstrap_sample_regression <- function(X, Y) {
+#'   if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
+#'   {
+#'     stop("X must be list or matrix or data frame!")
+#'   }
+#'   
+#'   if (!is.vector(Y))
+#'   {
+#'     stop("Y must be vector!")
+#'   }
+#'   n <- ncol(X)
+#'   if (n != length(Y))
+#'   {
+#'     stop("Dimension of x and y in data are not equal")
+#'   }
+#'   sample_indices <- sample(1:n, n, replace = TRUE)
+#'   return(list("X" = X[, sample_indices, drop = F], "Y" = Y[sample_indices]))
+#' }
+#' 
+#' # Function trains multiple regression models (decision trees) on different bootstrap 
+#' # samples. The number of models (`B`) can be specified:
+#' 
+#' #' @param X Data frame or matrix of predictors
+#' #' @param Y Vector of the response variable
+#' #' @param B Number of bootstrap samples/models to train (default is 100)
+#' #' @return List of trained regression models.
+#' #' @export
+#' #' @example
+#' 
+#' bagging_regression <- function(X, Y, B = 100) {
+#'   
+#'   if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
+#'   {
+#'     stop("X must be list or matrix or data frame!")
+#'   }
+#'   
+#'   if (!is.vector(Y))
+#'   {
+#'     stop("Y must be vector!")
+#'   }
+#'   
+#'   if(as.integer(B)!= B){
+#'     warning("B is not an integer. The value of B is set to ", trunc(B))
+#'     B <- trunc(B)
+#'   }
+#'   if(B < 1){
+#'     stop("Number of bootstrap samples must be greater than or equal to 1")
+#'   }
+#'   models <- list()
+#'   
+#'   
+#'   for (i in 1:B) {
+#'     #debug(bootstrap_sample)
+#'     sample_data <- bootstrap_sample_regression(X, Y)
+#'     X_boot = sample_data$X
+#'     Y_boot = sample_data$Y
+#'     #print(X_boot)
+#'     data <- list(x = X_boot, y = Y_boot)
+#'     model <- greedy_cart_regression(data, num_split = 5)# Training a single decision tree on each bootstrap sample
+#'     models[[i]] <- model
+#'   }
+#'   
+#'   return(models)
+#' }
+#' 
+#' # Function aggregates the predictions from all trained models by averaging them:
+#' #' @param models List of trained models from bagging_regression
+#' #' @param X Data frame or matrix of predictors to make predictions on
+#' #' @return Vector of aggregated predictions.
+#' #' @export
+#' #' @example
+#' 
+#' bagging_regression_prediction <- function(models, X) {
+#' 
+#'   if (!is.list(models))
+#'   {
+#'     stop("Models must be list!")
+#'   }
+#'   
+#'   if (!is.list(X) && !is.data.frame(X) && !is.matrix(X))
+#'   {
+#'     stop("X must be list or matrix or data frame!")
+#'   }
+#'   #print(models)
+#'   
+#'   preds <- sapply(models, function(model) {
+#'     #debug(prediction)
+#'     prediction(list_tree = list(model), list_x = X, type = 'reg')
+#'   })
+#'   
+#'   #print(preds)
+#'   return(rowMeans(preds))
+#' }
+#' 
+#' #' @examples
+#' #' models <- bagging_regression(X_train, Y_train, B = 1000)
+#' #' predictions <- bagging_prediction(models, X_test)
+#' 
+#' # The script generates synthetic data to demonstrate the bagging regression process:
+#' 
+#' #set.seed(123)
+#' #n <- 200
+#' #x <- runif(n, 0, 1)
+#' #y <- sin(2 * pi * x) + rnorm(n, 0, 0.1)
+#' #data <- list(x = matrix(x, nrow = 1, byrow = TRUE), y = y)
+#' 
+#' 
+#' # Two models are trained and visualized:
+#' # 
+#' # 1. Single Tree Model (`B = 1`):
+#' #    - Trains a single decision tree and visualizes the prediction against the true function.
+#' # 
+#' # 2. Bagging Model (`B = 1000`):
+#' #    - Trains 1000 decision trees and visualizes the aggregated prediction.
+#' 
+#' # Extract predictors and response variable from the data
+#' #X <- data$x
+#' #Y <- data$y
+#' #print(X)
+#' #print(Y)
+#' # Plotting layout
+#' #par(mfrow = c(2, 2))
+#' 
+#' # Training and plotting for B = 1 (Single Tree)
+#' #debug(random_forest)
+#' #debug(bootstrap_sample)
+#' #debug(find_best_split_regression)
+#' #single_model <- random_forest(x = x, y = y, type = 'reg', data = data, B = 1, A = 200, m = 1, num_leaf = 10, depth = NULL, num_split = 2, min_num = 1)#bagging_regression(X, Y, B = 1)
+#' #print("A")
+#' #print(single_model)
+#' #pred_single <- bagging_prediction(list(single_model), X)
+#' #plot(data$x, data$y, pch = 21, bg = 'lightgrey', main = "Bagging regression with 1 tree", xlab = "x1", ylab = "y")
+#' #lines(sort(data$x), pred_single[order(data$x)], col = 'blue')
+#' #lines(sort(data$x), sin(2 * pi * sort(data$x)), col = 'red')
+#' 
+#' # Training and plotting for B = 1000 (Bagging)
+#' #models <- bagging_regression(X, Y, B = 1000)
+#' #pred_bagging <- bagging_prediction(models, X)
+#' #plot(data$x, data$y, pch = 21, bg = 'lightgrey', main = "Bagging regression with 1000 trees", xlab = "x1", ylab = "y")
+#' #lines(sort(data$x), pred_bagging[order(data$x)], col = 'blue')
+#' #lines(sort(data$x), sin(2 * pi * sort(data$x)), col = 'red')
+#' #print_forest_splits(models)
+#' # Resetting the plotting area
+#' #par(mfrow = c(1, 1))
